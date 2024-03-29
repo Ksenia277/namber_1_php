159ed6e0c4eae81e58ee0ceeb8bed13d8e51a4cf
Ksenia277 <shahhovakseenis.2005@mail.ru>
2024-03-29 17:39:30 +0700

    Базовая защита от CSRF-атак

--- views/site/signup.php
+++ views/site/signup.php
@@ -1,11 +1,13 @@
 <div class="copies">
 <h2>Добавление нового библиотекаря</h2>
-<h3><?= $message ?? ''; ?></h3>
+<pre><?= $message ?? ''; ?></pre>
     <div class="signup">
     <form method="post">
+        <input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
+        <label>Имя <input type="text" name="name"></label>
         <label>Логин <input type="text" name="login"></label>
         <label>Пароль <input type="password" name="password"></label>
-        <button>Добавить</button>
+        <button>Добивить</button>
     </form>
     </div>
 </div>
