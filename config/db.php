8ab488c3626f846a61e0f34b4851b9860135f123
Ksenia277 <shahhovakseenis.2005@mail.ru>
2024-04-03 00:09:22 +0700

    ...

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'mvc',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'gtijvhez_m6',
+    'username' => 'gtijvhez',
+    'password' => 'm5iV3R',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
